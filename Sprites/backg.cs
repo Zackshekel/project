684e429a2e62e1146b6129c271219628abe205ac
Zackshekel <103376750+Zackshekel@users.noreply.github.com>
2022-04-10 17:18:08 +0100

    Add files via upload

---
+++ Sprites/backg.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace actual_computer_science_project.sprites
+
+{
+    public class Sprite : Component
+    {
+        protected Texture2D atexture;//texture png file for future sprites
+        public Vector2 Position { get; set; } // position of the sprite on the screen output
+        public Rectangle Rectangle
+        {
+            get { return new Rectangle((int)Position.X, (int)Position.Y, atexture.Width, atexture.Height); } // box around the sprite
+        }
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(atexture, Position, Color.White);
+        }
+        public Sprite(Texture2D texture)
+        {
+            atexture = texture;
+        }
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
---
+++ Sprites/player.cs
@@ -0,0 +1,43 @@
+﻿using actual_computer_science_project.sprites;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace actual_computer_science_project.Sprites
+{
+    public class Player : Sprite
+    {
+        public Player(Texture2D texture)
+            : base(texture) // inherits from the sprite class
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            var velocityspeed = new Vector2();
+            var speed = 1f; 
+            Position += velocityspeed; //variable to represent position since velocityspeed has an X and Y coordinate for later use
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+                velocityspeed.Y = -speed;
+            else if (Keyboard.GetState().IsKeyDown(Keys.A))
+                velocityspeed.X = -speed;
+            else if (Keyboard.GetState().IsKeyDown(Keys.S))
+                velocityspeed.Y = speed;
+            else if (Keyboard.GetState().IsKeyDown(Keys.D))
+                velocityspeed.X = speed;
+            
+        }
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+
+
+            base.Draw(gameTime, spriteBatch);
+        }
+    }
+}
